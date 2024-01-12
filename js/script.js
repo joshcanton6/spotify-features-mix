97cc9b495f4f72a408da9bb2cccf7400b296ff1a
joshcanton6 <60484057+joshcanton6@users.noreply.github.com>
2024-01-12 14:27:10 -0800

    Update script.js
    debug

--- js/script.js
+++ js/script.js
@@ -48,7 +48,5 @@ async function redirect() {
         sessionStorage.setItem("access_token", token.access_token);
         sessionStorage.setItem("refresh_token", token.refresh_token);
         sessionStorage.setItem("expires_at", Math.floor(Date.now() / 1000) + token.expires_in);
-
-        //window.location.href = home + "/form";
     }
 }
