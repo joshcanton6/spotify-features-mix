d903281a5bd4503794734bdf02f693281bf85884
joshcanton6 <60484057+joshcanton6@users.noreply.github.com>
2024-01-12 11:46:11 -0800

    Update script.js

---
+++ js/script.js
@@ -0,0 +1,19 @@
+client_id = "7cf492aacf9b49daa0367620797dc1fb";
+redirect_uri = "https://joshcanton6.github.io/spotify-features-mix/redirect";
+
+function login() {
+    const scope = "user-read-private" +
+        " user-read-email" +
+        " user-top-read" +
+        " playlist-read-private" +
+        " playlist-read-collaborative" +
+        " playlist-modify-public" +
+        " playlist-modify-private" +
+        " ugc-image-upload";
+
+    window.location.href = "https://accounts.spotify.com/authorize" +
+        "?client_id=" + client_id +
+        "&response_type=code" +
+        "&redirect_uri=" + encodeURIComponent(redirect_uri) +
+        "&scope=" + encodeURIComponent(scope);
+}
