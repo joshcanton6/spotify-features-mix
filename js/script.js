5f91bf09a1555e5fb9e2e76b0a746c78f6760468
Josh Canton <60484057+joshcanton6@users.noreply.github.com>
2024-06-21 09:41:15 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -31,10 +31,10 @@ async function redirect() {
 
         var token = await fetch("https://accounts.spotify.com/api/token", {
             method: "POST",
-            form: JSON.stringify({
-                "grant_type": "authorization_code",
-                "code": urlParams.get("code"),
-                "redirect_uri": redirect_uri
+            body: JSON.stringify({
+                grant_type: "authorization_code",
+                code: urlParams.get("code"),
+                redirect_uri: redirect_uri
             }), headers: {
                 "content-type": "application/x-www-form-urlencoded",
                 "Authorization": "Basic " + btoa(client_id + ":51ec60463d874c4c906640ee91c4146d")
